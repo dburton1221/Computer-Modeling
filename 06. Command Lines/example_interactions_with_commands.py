5736755db9e2a7a587dc00813f3c4098bf2286fe
David Burton <31712553+dburton1221@users.noreply.github.com>
2018-01-02 15:20:11 -0700

    Delete example_interactions_with_commands.py

--- 06. Command Lines/example_interactions_with_commands.py
+++
@@ -1,9 +0,0 @@
-import csv,sys
-print ("This is the name of the script: ", sys.argv[0])
-print ("Number of arguments: ", len(sys.argv))
-print ("The arguments are: " , str(sys.argv))
-if len(sys.argv)>1:
-    catch=""
-    for i in sys.argv:
-        catch=catch+i
-    print(catch)
